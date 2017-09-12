3c1a6d69665c788d333f075ae72d96268872278f
io53 <eliaseberg@gmail.com>
2017-09-12 21:12:19 +0300

    wrapped alarm in dbflow

--- app/src/main/java/com/ruuvi/tag/model/Alarm.java
+++ app/src/main/java/com/ruuvi/tag/model/Alarm.java
@@ -1,13 +1,31 @@
 package com.ruuvi.tag.model;
 
+import com.raizlabs.android.dbflow.annotation.Column;
+import com.raizlabs.android.dbflow.annotation.PrimaryKey;
+import com.raizlabs.android.dbflow.annotation.Table;
+import com.raizlabs.android.dbflow.structure.BaseModel;
+import com.ruuvi.tag.database.LocalDatabase;
+
 /**
  * Created by tmakinen on 26.7.2017.
  */
 
-public class Alarm {
+@Table(database = LocalDatabase.class)
+public class Alarm extends BaseModel {
+    @Column
+    @PrimaryKey(autoincrement = true)
+    public int id;
+    @Column
+    public String ruuviTagId;
+    @Column
     public int low;
+    @Column
     public int high;
+    @Column
     public String type;
+
+    public Alarm() {
+    }
 
     public Alarm(int low, int high, String type) {
         this.low = low;
