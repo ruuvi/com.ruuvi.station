3524d371b2f9be4be875c2a07f70689c8e638536
io53 <eliaseberg@gmail.com>
2017-09-20 21:55:38 +0300

    removed settings icon in toolbar

--- app/src/main/java/com/ruuvi/tag/feature/main/MainActivity.java
+++ app/src/main/java/com/ruuvi/tag/feature/main/MainActivity.java
@@ -51,9 +51,6 @@ public class MainActivity extends AppCompatActivity {
     private SharedPreferences settings;
     private List<RuuviTag> tags = new ArrayList<>();
 
-    public void openList(View view) {
-    }
-
     private void setTimerForAdvertise() {
         timer = new Timer();
         TimerTask updateProfile = new MainActivity.CustomTimerTask();
@@ -192,7 +189,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        //getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
 
