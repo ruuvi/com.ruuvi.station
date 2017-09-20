9ba3b72406613707e0318fc6a9ec4bc7696f54bc
io53 <eliaseberg@gmail.com>
2017-09-20 21:52:20 +0300

    added drawer some items

--- app/src/main/java/com/ruuvi/tag/feature/main/MainActivity.java
+++ app/src/main/java/com/ruuvi/tag/feature/main/MainActivity.java
@@ -13,11 +13,15 @@ import android.support.v4.content.ContextCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.view.menu.ExpandedMenuView;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import com.google.gson.Gson;
 
@@ -35,6 +39,7 @@ import com.ruuvi.tag.service.ScannerService;
 import com.ruuvi.tag.feature.settings.SettingsActivity;
 import com.ruuvi.tag.util.DeviceIdentifier;
 
+// TODO: 20/09/17 make this, settings and about into fragments 
 public class MainActivity extends AppCompatActivity {
     ScannerService service;
     private RuuviTagAdapter adapter;
@@ -47,8 +52,6 @@ public class MainActivity extends AppCompatActivity {
     private List<RuuviTag> tags = new ArrayList<>();
 
     public void openList(View view) {
-        Intent intent = new Intent(this, ListActivity.class);
-        startActivity(intent);
     }
 
     private void setTimerForAdvertise() {
@@ -97,6 +100,18 @@ public class MainActivity extends AppCompatActivity {
         getSupportActionBar().setHomeButtonEnabled(true);
         drawerToggle.syncState();
 
+        ListView drawerListView = findViewById(R.id.navigationDrawer_listView);
+
+        drawerListView.setAdapter(
+                new ArrayAdapter<>(
+                        this,
+                        android.R.layout.simple_list_item_1,
+                        getResources().getStringArray(R.array.navigation_items)
+                )
+        );
+
+        drawerListView.setOnItemClickListener(drawerItemClicked);
+
         gson = new Gson();
         text = findViewById(R.id.noTags_textView);
 
@@ -107,23 +122,31 @@ public class MainActivity extends AppCompatActivity {
         tags = RuuviTag.getAll();
         if (tags.size() > 0) findViewById(R.id.noTags_textView).setVisibility(View.GONE);
 
-        beaconListView = (ListView) findViewById(R.id.Tags_listView);
+        beaconListView = findViewById(R.id.Tags_listView);
         adapter = new RuuviTagAdapter(getApplicationContext(), tags);
         beaconListView.setAdapter(adapter);
 
         setTitle(R.string.title_activity_main);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab_add);
+        FloatingActionButton fab = findViewById(R.id.fab_add);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                openList(view);
             }
         });
 
         setTimerForAdvertise();
         adapter.notifyDataSetChanged();
     }
+
+    AdapterView.OnItemClickListener drawerItemClicked = new AdapterView.OnItemClickListener() {
+        @Override
+        public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+            // TODO: 20/09/17 make this open differnt fragments 
+            Intent intent = new Intent(MainActivity.this, ListActivity.class);
+            startActivity(intent);
+        }
+    };
 
     @Override
     protected void onStart() {
