7cf439ed1f07fffba3f029a973e9862db625ab75
io53 <eliaseberg@gmail.com>
2017-09-23 19:39:06 +0300

    show first letter of tag name in ball

--- app/src/main/java/com/ruuvi/tag/adapters/RuuviTagAdapter.java
+++ app/src/main/java/com/ruuvi/tag/adapters/RuuviTagAdapter.java
@@ -3,8 +3,6 @@ package com.ruuvi.tag.adapters;
 import android.content.Context;
 import android.content.Intent;
 import android.graphics.Color;
-import android.net.Uri;
-import android.support.annotation.LayoutRes;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
@@ -12,13 +10,14 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.ruuvi.tag.R;
 import com.ruuvi.tag.feature.edit.EditActivity;
-import com.ruuvi.tag.feature.main.MainActivity;
 import com.ruuvi.tag.feature.plot.PlotActivity;
 import com.ruuvi.tag.model.RuuviTag;
+import com.ruuvi.tag.util.Utils;
 
 import java.util.Date;
 import java.util.List;
@@ -94,6 +93,12 @@ public class RuuviTagAdapter extends ArrayAdapter<RuuviTag> {
                 getContext().startActivity(intent);
             }
         });
+
+        ((ImageView)convertView.findViewById(R.id.row_main_letter))
+                .setImageBitmap(Utils.createBall(100,
+                        R.color.ap_gray,
+                        Color.WHITE,
+                        txtId.getText().charAt(0) + ""));
         return convertView;
     }
 }
