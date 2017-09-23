d648106efb0dab786f2a6859ed34fb9a70ed41a0
io53 <eliaseberg@gmail.com>
2017-09-23 21:11:53 +0300

    show version in about screen

--- app/src/main/java/com/ruuvi/tag/feature/main/AboutFragment.java
+++ app/src/main/java/com/ruuvi/tag/feature/main/AboutFragment.java
@@ -6,7 +6,9 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
+import com.ruuvi.tag.BuildConfig;
 import com.ruuvi.tag.R;
 
 public class AboutFragment extends Fragment {
@@ -28,6 +30,10 @@ public class AboutFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_about, container, false);
+
+        ((TextView)view.findViewById(R.id.versionInfo))
+                .setText("Version: " + BuildConfig.VERSION_NAME
+                        + " (" + BuildConfig.VERSION_CODE + ")");
 
         return view;
     }
