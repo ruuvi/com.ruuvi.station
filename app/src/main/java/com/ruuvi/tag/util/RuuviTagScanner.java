717d490295cf33f66a6c7f01a63465438509a5d1
io53 <eliaseberg@gmail.com>
2017-09-30 09:03:04 +0300

    added empty tag scanner class and listener interface

---
+++ app/src/main/java/com/ruuvi/tag/util/RuuviTagScanner.java
@@ -0,0 +1,17 @@
+package com.ruuvi.tag.util;
+
+public class RuuviTagScanner {
+    private RuuviTagListener listener;
+
+    RuuviTagScanner(RuuviTagListener listener) {
+        this.listener = listener;
+    }
+
+    public void start() {
+
+    }
+
+    public void stop() {
+
+    }
+}
