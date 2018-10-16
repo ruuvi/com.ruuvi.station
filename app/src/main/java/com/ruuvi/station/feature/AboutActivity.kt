facf2b619cfef46145ebb0fb8be94c9c257605e7
io53 <->
2018-10-16 20:44:28 +0300

    temporary debug feature

--- app/src/main/java/com/ruuvi/station/feature/AboutActivity.kt
+++ app/src/main/java/com/ruuvi/station/feature/AboutActivity.kt
@@ -1,14 +1,17 @@
 package com.ruuvi.station.feature
 
 import android.os.Bundle
+import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
 import android.text.method.LinkMovementMethod
 import android.view.MenuItem
+import android.widget.Toast
 import com.ruuvi.station.BuildConfig
 import com.ruuvi.station.R
 
 import kotlinx.android.synthetic.main.activity_about.*
 import kotlinx.android.synthetic.main.content_about.*
+import java.io.File
 
 class AboutActivity : AppCompatActivity() {
 
@@ -23,6 +26,19 @@ class AboutActivity : AppCompatActivity() {
 
         aboutInfoText.movementMethod = LinkMovementMethod.getInstance()
         versionInfo.text = getString(R.string.version) + ": " + BuildConfig.VERSION_NAME
+
+        //todo: remove me
+        val path = application.filesDir.path + "/android-beacon-library-scan-state"
+        val file = File(path)
+        val snack = Snackbar.make(about_root, "Size: " + file.length() / 1024 + "KB", Snackbar.LENGTH_INDEFINITE)
+        snack.setAction("Remove") {
+            if (file.delete()) {
+               Toast.makeText(this, "Deleted!", Toast.LENGTH_SHORT).show()
+            } else {
+                Toast.makeText(this, "Could not delete", Toast.LENGTH_SHORT).show()
+            }
+        }
+        snack.show()
     }
 
     override fun onOptionsItemSelected(item: MenuItem?): Boolean {
