209992d66598018427904ce9927176409ebf3af7
AJ <>
2020-01-23 18:55:09 +0800

    [CHANGED] annotate ruuvi range notifier

--- bluetooth_library/src/main/java/com/ruuvi/station/bluetooth/interfaces/IRuuviRangeNotifier.kt
+++ bluetooth_library/src/main/java/com/ruuvi/station/bluetooth/interfaces/IRuuviRangeNotifier.kt
@@ -11,12 +11,24 @@ interface IRuuviRangeNotifier {
 
     fun stopScanning()
 
+    /*
+     * Enable background scanning
+     */
     fun enableBackgroundMode(isBackgroundModeEnabled: Boolean)
 
+    /*
+     * Get interval between scan jobs in milliseconds if it is already set
+     */
     fun getBackgroundScanInterval(): Long?
 
+    /*
+     * Set interval between scan jobs in milliseconds
+     */
     fun setBackgroundScheduledScanInterval(scanInterval: Long)
 
+    /*
+    * Enable scheduled scans every *setBackgroundScheduledScanInterval* milliseconds
+    */
     fun enableScheduledScans(areScheduledScansEnabled: Boolean)
 
     interface OnTagsFoundListener {
