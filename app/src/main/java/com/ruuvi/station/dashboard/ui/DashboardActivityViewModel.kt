de558564e2d76b6691b74a8a235c25066aeeba23
Denis Andreev <andreev.denis@gmail.com>
2020-09-25 14:59:36 +0300

    ISSUE ANDROID-172  1.2.8 Bell on dashboard has gone quite bonkers [FIXED] bell blinking fixed [skip ci]

--- app/src/main/java/com/ruuvi/station/dashboard/ui/DashboardActivityViewModel.kt
+++ app/src/main/java/com/ruuvi/station/dashboard/ui/DashboardActivityViewModel.kt
@@ -1,43 +1,39 @@
 package com.ruuvi.station.dashboard.ui
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.ruuvi.station.bluetooth.BluetoothInteractor
 import com.ruuvi.station.tag.domain.RuuviTag
 import com.ruuvi.station.tag.domain.TagInteractor
 import com.ruuvi.station.units.domain.UnitsConverter
-import kotlinx.coroutines.ExperimentalCoroutinesApi
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import java.util.Timer
 import kotlin.concurrent.scheduleAtFixedRate
 
-@ExperimentalCoroutinesApi
 class DashboardActivityViewModel(
-    private val bluetoothInteractor: BluetoothInteractor,
     private val tagInteractor: TagInteractor,
     val converter: UnitsConverter
 ) : ViewModel() {
 
-    private val tags = MutableStateFlow<List<RuuviTag>>(arrayListOf())
-    val tagsFlow: StateFlow<List<RuuviTag>> = tags
+    private val tags = MutableLiveData<List<RuuviTag>>(arrayListOf())
+    val observeTags: LiveData<List<RuuviTag>> = tags
 
-    private val flowTimer = Timer("DashboardActivityViewModelTimer", false)
+    private val getTagsTimer = Timer("DashboardActivityViewModelTimer", false)
 
     init {
         getTagsFlow()
     }
 
-    fun startForegroundScanning() {
-        if (bluetoothInteractor.canScan())
-            bluetoothInteractor.startForegroundScanning()
-    }
-
     private fun getTagsFlow() {
-        viewModelScope.launch {
-            flowTimer.scheduleAtFixedRate(0, 500) {
-                tags.value = tagInteractor.getTags()
+        getTagsTimer.scheduleAtFixedRate(0, 1000) {
+            viewModelScope.launch {
+                val getTags = tagInteractor.getTags()
+                withContext(Dispatchers.Main) {
+                    tags.value = getTags
+                }
             }
         }
     }
