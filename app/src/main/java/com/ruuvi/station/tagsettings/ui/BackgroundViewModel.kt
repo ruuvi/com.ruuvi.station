a626c84c82069522d88ac567701e18165fbe5ac2
Denis Andreev <andreev.denis@gmail.com>
2023-01-09 12:16:32 +0300

    ISSUE #854 New dashboard layout [FIXED] sync image right after background change

--- app/src/main/java/com/ruuvi/station/tagsettings/ui/BackgroundViewModel.kt
+++ app/src/main/java/com/ruuvi/station/tagsettings/ui/BackgroundViewModel.kt
@@ -2,25 +2,35 @@ package com.ruuvi.station.tagsettings.ui
 
 import android.net.Uri
 import androidx.lifecycle.ViewModel
+import com.ruuvi.station.network.domain.NetworkDataSyncInteractor
 import com.ruuvi.station.tagsettings.domain.TagSettingsInteractor
 import java.io.File
 
 class BackgroundViewModel(
     val sensorId: String,
-    val tagSettingsInteractor: TagSettingsInteractor
+    val tagSettingsInteractor: TagSettingsInteractor,
+    val networkDataSyncInteractor: NetworkDataSyncInteractor
 ): ViewModel() {
 
     fun getDefaultImages(): List<Int> = tagSettingsInteractor.getDefaultImages()
 
-    fun setDefaultImage(resource: Int) =
+    fun setDefaultImage(resource: Int) {
         tagSettingsInteractor.setDefaultBackgroundImageByResource(sensorId, resource)
+        requestCloudSync()
+    }
 
     fun setImageFromGallery(uri: Uri) {
         tagSettingsInteractor.setImageFromGallery(sensorId, uri)
+        requestCloudSync()
     }
 
     fun setImageFromCamera(imageFile: File, uri: Uri) {
         tagSettingsInteractor.setImageFromCamera(sensorId, imageFile, uri)
+        requestCloudSync()
+    }
+
+    private fun requestCloudSync() {
+        networkDataSyncInteractor.syncNetworkData()
     }
 
     fun getImageFileForCamera(): Pair<File, Uri> = tagSettingsInteractor.createFileForCamera(sensorId)
