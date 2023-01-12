d59e54a114574dbc8f03ba675e9437acc57e8b42
Denis Andreev <andreev.denis@gmail.com>
2023-01-12 22:36:52 +0300

    Push tests

--- app/src/main/java/com/ruuvi/station/firebase/domain/RuuviFirebaseMessagingService.kt
+++ app/src/main/java/com/ruuvi/station/firebase/domain/RuuviFirebaseMessagingService.kt
@@ -1,7 +1,15 @@
 package com.ruuvi.station.firebase.domain
 
+import android.app.Notification
+import android.graphics.BitmapFactory
+import androidx.core.app.NotificationCompat
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
+import com.ruuvi.station.R
+import com.ruuvi.station.alarm.domain.AlarmCheckInteractor
+import com.ruuvi.station.alarm.receiver.CancelAlarmReceiver
+import com.ruuvi.station.alarm.receiver.MuteAlarmReceiver
+import com.ruuvi.station.tagdetails.ui.TagDetailsActivity
 import timber.log.Timber
 import java.util.*
 
@@ -42,4 +50,81 @@ class RuuviFirebaseMessagingService: FirebaseMessagingService()  {
         // Also if you intend on generating your own notifications as a result of a received FCM
         // message, here is where that should be initiated. See sendNotification method below.
     }
+
+
+    private fun createNotification(checker: AlarmCheckInteractor.AlarmChecker): Notification? {
+        val message = checker.getMessage()
+        if (!message.isNullOrEmpty()) {
+            val tagDetailsPendingIntent =
+                TagDetailsActivity.createPendingIntent(
+                    this,
+                    checker.ruuviTag.id,
+                    checker.alarm.id
+                )
+            val cancelPendingIntent =
+                CancelAlarmReceiver.createPendingIntent(this, checker.alarm.id)
+            val mutePendingIntent = MuteAlarmReceiver.createPendingIntent(this, checker.alarm.id)
+            val action = NotificationCompat.Action(
+                R.drawable.ic_ruuvi_app_notification_icon_v2,
+                this.getString(R.string.alarm_notification_disable),
+                cancelPendingIntent
+            )
+            val actionMute = NotificationCompat.Action(
+                R.drawable.ic_ruuvi_app_notification_icon_v2,
+                this.getString(R.string.alarm_mute_for_hour),
+                mutePendingIntent
+            )
+
+            val bitmap = BitmapFactory.decodeResource(this.resources, R.mipmap.ic_launcher)
+            return NotificationCompat
+                .Builder(this, "notify_001")//AlarmCheckInteractor.CHANNEL_ID)
+                .setContentTitle(message)
+                .setTicker("${checker.ruuviTag.displayName} $message")
+                .setStyle(
+                    NotificationCompat
+                        .BigTextStyle()
+                        .setBigContentTitle(message)
+                        .setSummaryText(checker.ruuviTag.displayName)
+                        .bigText(checker.alarm.customDescription)
+                )
+                .setContentText(checker.alarm.customDescription)
+                .setDefaults(Notification.DEFAULT_ALL)
+                .setOnlyAlertOnce(true)
+                .setAutoCancel(true)
+                .setPriority(NotificationCompat.PRIORITY_MAX)
+                .setContentIntent(tagDetailsPendingIntent)
+                .setLargeIcon(bitmap)
+                .setSmallIcon(R.drawable.ic_ruuvi_app_notification_icon_v2)
+                .addAction(action)
+                .addAction(actionMute)
+                .build()
+        }
+        return null
+    }
 }
+
+/*
+        val jsonString = """
+            {default=
+            	{
+            	"token": "aadf4frwfiosjfosjflsafjlsafjlsknfdlnsvl",
+            	"email": "denis@ruuvi.com",
+                "type": "alert"
+            	"data":
+            		{
+            			"name": "Fridge",
+                        "id": "AA:BB:CC:DD:EE:FF",
+                        "alertType": "Temperature",
+                        "triggerType": "Over",
+                        "currentValue": "14",
+                        "thresholdValue": "10",
+                        "alertUnit": "C",
+                        "alertData": "Fridge is too warm!"
+                    }
+                }
+            }
+        """.trimIndent()
+        val ob = GsonBuilder().create().fromJson(jsonString, AlertPush::class.java)
+
+        Timber.d("parsed $ob")
+ */
