c5ce8098561e31d02af3852847cd3ada5c1edb05
Denis Andreev <andreev.denis@gmail.com>
2023-04-19 14:49:16 +0300

    ISSUE #987 2.1.7 App crash when tap down arrow for air pressure alert

--- app/src/main/java/com/ruuvi/station/app/ui/components/RangeSlider.kt
+++ app/src/main/java/com/ruuvi/station/app/ui/components/RangeSlider.kt
@@ -1,12 +1,13 @@
 package com.ruuvi.station.app.ui.components
 
-import androidx.annotation.FloatRange
 import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.material.RangeSlider
 import androidx.compose.material.SliderDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import com.ruuvi.station.app.ui.theme.RuuviStationTheme
+import java.lang.Float.max
+import java.lang.Float.min
 
 @Composable
 fun RuuviSliderColors() = SliderDefaults.colors(
@@ -26,8 +27,10 @@ fun RuuviRangeSlider(
     steps: Int = 0,
     onValueChangeFinished: (() -> Unit)? = null,
 ) {
+    val start = if (valueRange.contains(values.start)) values.start else valueRange.start
+    val end = if (valueRange.contains(values.endInclusive)) values.endInclusive else valueRange.endInclusive
     RangeSlider(
-        value = values.start..values.endInclusive,
+        value = min(start, end)..max(start,end),
         onValueChange = {
             onValueChange.invoke(it.start..it.endInclusive)
         },
