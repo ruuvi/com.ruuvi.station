adfc9d9a768c665b3ff26893215e25d4a65ef51b
Denis <andreev.denis@gmail.com>
2024-04-30 00:53:08 +0300

    Update version_increment.sh

--- scripts/ci/version_increment.sh
+++ scripts/ci/version_increment.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
 
 # Read the current version name from build.gradle
-current_version_name=$(grep -oP 'versionName "\K[^"]+' app/build.gradle)
+current_version_name=$(grep -oP 'versionName "\K\d+\.\d+\.\d+' app/build.gradle)
 
-# Increment the version name
-incremented_version_name=$(echo $current_version_name | awk -F '.' '{$NF++; OFS="."; print $0}')
+# Extract major, minor, and patch version numbers
+major_version=$(echo $current_version_name | cut -d'.' -f1)
+minor_version=$(echo $current_version_name | cut -d'.' -f2)
+patch_version=$(echo $current_version_name | cut -d'.' -f3)
+
+# Increment the patch version
+((patch_version++))
+
+# Construct the incremented version name
+incremented_version_name="$major_version.$minor_version.$patch_version"
 
 # Update the version name in build.gradle
 sed -i "s/versionName \"$current_version_name\"/versionName \"$incremented_version_name\"/" app/build.gradle
