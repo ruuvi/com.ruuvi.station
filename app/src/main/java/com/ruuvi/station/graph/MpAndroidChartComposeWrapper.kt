b40c938e756a9fd6cd4a6c2538a259c3c698712a
Denis Andreev <andreev.denis@gmail.com>
2024-09-05 13:30:33 +0300

    ISSUE #1311 Tablet UI issues Fix for dot radius

--- app/src/main/java/com/ruuvi/station/graph/MpAndroidChartComposeWrapper.kt
+++ app/src/main/java/com/ruuvi/station/graph/MpAndroidChartComposeWrapper.kt
@@ -484,7 +484,7 @@ private fun addDataToChart(
     set.setDrawValues(false)
     set.setDrawFilled(true)
     set.lineWidth = 1f
-    set.circleRadius = 1.1f
+    set.circleRadius = 1.2f
     set.color = ContextCompat.getColor(context, R.color.chartLineColor)
     set.setCircleColor(ContextCompat.getColor(context, R.color.chartLineColor))
     set.fillColor = ContextCompat.getColor(context, R.color.chartFillColor)
