6b23df3c2d2cf4d22578ce10637f940e1f15b4cb
Denis Andreev <andreev.denis@gmail.com>
2025-03-05 13:33:12 +0300

    AQI progress fix

--- app/src/main/java/com/ruuvi/station/app/ui/components/AqiProgress.kt
+++ app/src/main/java/com/ruuvi/station/app/ui/components/AqiProgress.kt
@@ -1,28 +1,25 @@
 package com.ruuvi.station.app.ui.components
 
-import androidx.compose.animation.core.LinearEasing
-import androidx.compose.animation.core.RepeatMode
-import androidx.compose.animation.core.animateFloat
-import androidx.compose.animation.core.infiniteRepeatable
-import androidx.compose.animation.core.rememberInfiniteTransition
-import androidx.compose.animation.core.tween
 import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.geometry.Rect
 import androidx.compose.ui.geometry.Size
-import androidx.compose.ui.graphics.BlendMode
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.StrokeCap
 import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import com.ruuvi.station.app.ui.theme.ruuviStationFonts
 import com.ruuvi.station.util.extensions.scaledSp
@@ -33,39 +30,29 @@ import kotlin.math.sin
 fun CircularGradientProgress(
     progress: Float,
     lineColor: Color,
+    size: Dp = 130.dp,
     modifier: Modifier = Modifier
 ) {
     Box(
         contentAlignment = Alignment.Center,
-        modifier = modifier.size(120.dp) // Adjust size as needed
+        modifier = modifier.size(size)
     ) {
 
-        val infiniteTransition = rememberInfiniteTransition(label = "")
-
-        val glowAlpha by infiniteTransition.animateFloat(
-            initialValue = 0.4f,
-            targetValue = 0.9f,
-            animationSpec = infiniteRepeatable(
-                animation = tween(1000, easing = LinearEasing),
-                repeatMode = RepeatMode.Restart
-            ),
-            label = ""
-        )
-
-        Canvas(modifier = modifier.size(150.dp)) {
+        Canvas(modifier = Modifier.fillMaxSize().padding(8.dp)) {
             val strokeWidth = 20f
             val size = this.size.minDimension
             val radius = size / 2 //- strokeWidth / 2
-            val center = Offset(size / 2, size / 2)
+
             val startAngle = 135f  // Starting point for 270-degree arc
             val sweepAngle = 270f * (progress / 100f)
             val rect = Rect(Offset.Zero, Size(size, size))
+            val dotRadius = 20f / 2
 
 
             // Draw background arc
             drawArc(
                 color = Color.Black.copy(alpha = 0.8f),
-                startAngle = 135f,
+                startAngle = startAngle,
                 sweepAngle = 270f,
                 useCenter = false,
                 style = Stroke(width = strokeWidth, cap = StrokeCap.Square)
@@ -73,51 +60,127 @@ fun CircularGradientProgress(
 
             // Draw progress arc with gradient
             drawArc(
-                color = lineColor,
-                startAngle = 135f,
+                color = lineColor.copy(alpha = 0.8f),
+                startAngle = startAngle,
                 sweepAngle = sweepAngle,
                 useCenter = false,
                 style = Stroke(width = strokeWidth, cap = StrokeCap.Square)
             )
 
-            val angleInRadians = Math.toRadians((135 + (270 * progress / 100f)).toDouble()).toFloat()
+            val angleInRadians = Math.toRadians((startAngle + (270 * progress / 100f) + (if (progress == 0f) -2.5 else 2.5))).toFloat()
             val dotX = rect.center.x + (radius ) * cos(angleInRadians)
             val dotY = rect.center.y + (radius ) * sin(angleInRadians)
 
-
-            // Draw the glowing dot
             drawCircle(
-                color = lineColor.copy(alpha = glowAlpha), // Outer glow effect
-                radius = 12f * 1.5f,
-                center = Offset(dotX, dotY),
-                blendMode = BlendMode.Screen
-            )
+                brush = Brush.radialGradient(
+                    colors = listOf(
+                        lineColor,
+                        lineColor.copy(alpha = 0.8f),
 
-            drawCircle(
-                color = lineColor, // Solid inner circle
-                radius = 12f,
+                        lineColor.copy(alpha = 0.5f),
+                        lineColor.copy(alpha = 0.3f),
+                        Color.Transparent
+                    ),
+                radius = dotRadius * 2.2f,
+                center = Offset(dotX, dotY)
+                ),
+                radius = dotRadius * 2.2f,
                 center = Offset(dotX, dotY)
             )
 
+            // Solid inner circle
+            drawCircle(
+                color = lineColor,
+                radius = dotRadius,
+                center = Offset(dotX, dotY)
+            )
+
+
         }
+
         // Centered Label
         Text(
             text = progress.toInt().toString(),
-            fontSize = 60.scaledSp,
+            fontSize = 54.scaledSp,
             fontFamily = ruuviStationFonts.oswaldBold,
             fontWeight = FontWeight.Bold,
             color = Color.White,
             modifier = Modifier.padding(bottom = 8.dp)
         )
 
-        Box(contentAlignment = Alignment.BottomCenter, modifier = Modifier.align(Alignment.BottomCenter))
+        Box(contentAlignment = Alignment.BottomCenter,
+            modifier = Modifier
+                .align(Alignment.BottomCenter)
+                .padding(bottom = 4.dp)
+        )
         {
             Text(
                 text = "/100",
-                fontSize = 20.scaledSp,
+                fontSize = 18.scaledSp,
                 fontFamily = ruuviStationFonts.oswaldRegular,
                 color = Color.White
             )
         }
     }
+}
+
+
+@Preview
+@Composable
+fun CircularGradientProgressPreview0(
+    modifier: Modifier = Modifier.background(color = Color(0xFF083C3D))
+) {
+    CircularGradientProgress(
+        progress = 0f,
+        lineColor = Color.Red,
+        modifier = modifier
+    )
+}
+
+@Preview
+@Composable
+fun CircularGradientProgressPreview25(
+    modifier: Modifier = Modifier.background(color = Color(0xFF083C3D))
+) {
+    CircularGradientProgress(
+        progress = 25f,
+        lineColor = Color.Red,
+        modifier = modifier
+    )
+}
+
+@Preview
+@Composable
+fun CircularGradientProgressPreview(
+    modifier: Modifier = Modifier.background(color = Color(0xFF083C3D))
+) {
+    CircularGradientProgress(
+        progress = 50f,
+        lineColor = Color.Yellow,
+        modifier = modifier
+    )
+}
+
+@Preview
+@Composable
+fun CircularGradientProgressPreview75(
+    modifier: Modifier = Modifier.background(color = Color(0xFF083C3D))
+) {
+    CircularGradientProgress(
+        progress = 75f,
+        lineColor = Color.Green,
+        modifier = modifier
+    )
+}
+
+@Preview
+@Composable
+fun CircularGradientProgressPreview100(
+    modifier: Modifier = Modifier.background(color = Color(0xFF083C3D))
+) {
+    CircularGradientProgress(
+        progress = 100f,
+        lineColor = Color.Green,
+        modifier = modifier
+    )
 }
