52a183f0f358a58eb94c46adf0a1e424d9b8f49f
Denis Andreev <andreev.denis@gmail.com>
2025-05-31 03:42:12 +0300

    ISSUE #1381 Support masonry layout for dashboard

--- app/src/main/java/com/ruuvi/station/dashboard/ui/ItemStaggeredGridDragAndDropState.kt
+++ app/src/main/java/com/ruuvi/station/dashboard/ui/ItemStaggeredGridDragAndDropState.kt
@@ -2,10 +2,6 @@ package com.ruuvi.station.dashboard.ui
 
 import androidx.compose.foundation.gestures.detectDragGesturesAfterLongPress
 import androidx.compose.foundation.gestures.scrollBy
-import androidx.compose.foundation.lazy.LazyListItemInfo
-import androidx.compose.foundation.lazy.grid.LazyGridItemInfo
-import androidx.compose.foundation.lazy.grid.LazyGridState
-import androidx.compose.foundation.lazy.grid.rememberLazyGridState
 import androidx.compose.foundation.lazy.staggeredgrid.LazyStaggeredGridItemInfo
 import androidx.compose.foundation.lazy.staggeredgrid.LazyStaggeredGridState
 import androidx.compose.foundation.lazy.staggeredgrid.rememberLazyStaggeredGridState
@@ -21,6 +17,7 @@ import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.unit.IntSize
+import com.ruuvi.station.tagsettings.ui.offsetEnd
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.launch
@@ -33,17 +30,23 @@ fun Modifier.dragGestureHandler(
 ): Modifier = this.pointerInput(Unit) {
     detectDragGesturesAfterLongPress(
         onDrag = { change, offset ->
-            Timber.d("dragGestureHandler - onDrag")
+            Timber.d("dragGestureHandler - onDrag $offset")
             change.consume()
             itemStaggeredGridDragAndDropState.onDrag(offset)
             handleOverscrollJob(overscrollJob, scope, itemStaggeredGridDragAndDropState)
         },
-        onDragStart = { offset -> itemStaggeredGridDragAndDropState.onDragStart(offset)
-            Timber.d("dragGestureHandler - onDragStart $offset") },
-        onDragEnd = { itemStaggeredGridDragAndDropState.onDragInterrupted(false)
-            Timber.d("dragGestureHandler - onDragEnd") },
-        onDragCancel = { itemStaggeredGridDragAndDropState.onDragInterrupted(true)
-            Timber.d("dragGestureHandler - onDragCancel") }
+        onDragStart = { offset ->
+            Timber.d("dragGestureHandler - onDragStart $offset")
+            itemStaggeredGridDragAndDropState.onDragStart(offset)
+                      },
+        onDragEnd = {
+            Timber.d("dragGestureHandler - onDragEnd")
+            itemStaggeredGridDragAndDropState.onDragInterrupted(false)
+                    },
+        onDragCancel = {
+            Timber.d("dragGestureHandler - onDragCancel")
+            itemStaggeredGridDragAndDropState.onDragInterrupted(true)
+                       },
     )
 }
 
@@ -68,7 +71,9 @@ fun rememberDragDropStaggeredGridState(
     lazyListState: LazyStaggeredGridState = rememberLazyStaggeredGridState(),
     onMove: (Int, Int) -> Unit,
     onDoneDragging: () -> Unit): ItemStaggeredGridDragAndDropState {
-    return remember { ItemStaggeredGridDragAndDropState(lazyListState, onMove, onDoneDragging) }
+    return remember {
+        ItemStaggeredGridDragAndDropState(lazyListState, onMove, onDoneDragging)
+    }
 }
 class ItemStaggeredGridDragAndDropState(
     private val lazyListState: LazyStaggeredGridState,
@@ -76,7 +81,7 @@ class ItemStaggeredGridDragAndDropState(
     private val onDoneDragging: () -> Unit
 ) {
     private var draggedDistance by mutableStateOf(Offset.Zero)
-    private var initialOffset by mutableStateOf(Offset.Zero)
+    private var dragStartPoint by mutableStateOf(Offset.Zero)
     private var initiallyDraggedElement by mutableStateOf<LazyStaggeredGridItemInfo?>(null)
     private var currentIndexOfDraggedItem by mutableIntStateOf(-1)
     private var overscrollJob by mutableStateOf<Job?>(null)
@@ -104,56 +109,28 @@ class ItemStaggeredGridDragAndDropState(
     // Functions for handling drag gestures
     fun onDragStart(offset: Offset) {
         isDragInProgress = true
-        initialOffset = offset
+        dragStartPoint = offset
         for (item in lazyListState.layoutInfo.visibleItemsInfo) {
-            Timber.d("dragGestureHandler - visible item ${item.offset} ${item.size}")
+            Timber.d("dragGestureHandler onDragStart - visible item ${item.offset} ${item.size}")
         }
         lazyListState.layoutInfo.visibleItemsInfo
             .firstOrNull { item -> offset.y.toInt() in item.offset.y..(item.offset.y + item.size.height)
                     && offset.x.toInt() in item.offset.x..(item.offset.x + item.size.width)
             }
             ?.also {
-                Timber.d("dragGestureHandler - currentIndexOfDraggedItem ${it.index}")
+                Timber.d("dragGestureHandler onDragStart - currentIndexOfDraggedItem ${it.index}")
                 currentIndexOfDraggedItem = it.index
                 initiallyDraggedElement = it
             }
     }
 
-    // Handle interrupted drag gesture
-    fun onDragInterrupted(canceled: Boolean) {
-        isDragInProgress = false
-        initialOffset = Offset.Unspecified
-        draggedDistance = Offset.Zero
-        currentIndexOfDraggedItem = -1
-        initiallyDraggedElement = null
-        overscrollJob?.cancel()
-        if (!canceled) onDoneDragging.invoke()
-    }
-
-    // Helper function to calculate start and end offsets
-    // Calculate the start and end offsets of the dragged element
-//    private fun calculateOffsets(offset: Float): Pair<Float, Float> {
-//        val startOffset = offset + draggedDistance
-//        val currentElementSize = currentElement?.size ?: IntSize.Zero
-//        val endOffset = offset + draggedDistance + currentElementSize
-//        return startOffset to endOffset
-//    }
-
-    private fun calculateOffsets(offset: IntOffset): Pair<IntOffset, IntOffset> {
-        val startOffset = offset + IntOffset(draggedDistance.x.toInt(), draggedDistance.y.toInt())
-        val currentElementSize = currentElement?.size ?: IntSize.Zero
-        val endOffset = offset + IntOffset(draggedDistance.x.toInt(), draggedDistance.y.toInt()) + IntOffset(currentElementSize.width, currentElementSize.height)
-        return startOffset to endOffset
-    }
-
     fun onDrag(offset: Offset) {
-
         draggedDistance += offset
-        Timber.d("onDrag offset $offset draggedDistance $draggedDistance")
+        Timber.d("dragGestureHandler onDrag draggedDistance $draggedDistance")
         val initial = initialOffsets ?: return
         val (startOffset, endOffset) = calculateOffsets(initial)
 
-        val fingerOffset = initialOffset + draggedDistance
+        val fingerOffset = dragStartPoint + draggedDistance
 
         val hoveredElement = currentElement
         if (hoveredElement != null) {
@@ -161,9 +138,17 @@ class ItemStaggeredGridDragAndDropState(
                 hoveredElement.index != item.index && item.pointInside(fingerOffset)
             }
 
-            val targetItem = validItems.sortedBy { it.offset.y }.firstOrNull()
+            val targetItem = validItems.firstOrNull { item ->
+                val delta = startOffset - hoveredElement.offset
+
+                when {
+                    delta.y > 0 -> (endOffset.y > item.offset.y)
+                    else -> (startOffset.y < item.offset.y)
+                }
+            }
 
             if (targetItem != null) {
+                Timber.d("dragGestureHandler - onDrag targetItem $targetItem")
                 currentIndexOfDraggedItem.let { current ->
                     onMove.invoke(current, targetItem.index)
                     currentIndexOfDraggedItem = targetItem.index
@@ -172,27 +157,48 @@ class ItemStaggeredGridDragAndDropState(
         }
     }
 
+    // Handle interrupted drag gesture
+    fun onDragInterrupted(canceled: Boolean) {
+        Timber.d("dragGestureHandler - onDragInterrupted $canceled")
+        isDragInProgress = false
+        dragStartPoint = Offset.Unspecified
+        draggedDistance = Offset.Zero
+        currentIndexOfDraggedItem = -1
+        initiallyDraggedElement = null
+        overscrollJob?.cancel()
+        if (!canceled) onDoneDragging.invoke()
+    }
+
+    private fun calculateOffsets(offset: IntOffset): Pair<IntOffset, IntOffset> {
+        val startOffset = offset + IntOffset(draggedDistance.x.toInt(), draggedDistance.y.toInt())
+        val currentElementSize = currentElement?.size ?: IntSize.Zero
+        val endOffset = offset + IntOffset(draggedDistance.x.toInt(), draggedDistance.y.toInt()) + IntOffset(currentElementSize.width, currentElementSize.height)
+        return startOffset to endOffset
+    }
+
     fun LazyStaggeredGridItemInfo.pointInside(offset: Offset): Boolean {
         return offset.x >= this.offset.x && offset.x < (this.offset.x + this.size.width) &&
                 offset.y >= this.offset.y && offset.y < (this.offset.y + this.size.height)
     }
 
     fun checkForOverScroll(): Float {
-        val draggedElement = initiallyDraggedElement
-        if (draggedElement != null) {
-            val (startOffset, endOffset) = calculateOffsets(draggedElement.offset)
-            Timber.d("lazyListState ${lazyListState.layoutInfo.viewportEndOffset} ${lazyListState.layoutInfo.viewportStartOffset}")
-            Timber.d("lazyListState startOffset, endOffset ${startOffset} ${endOffset}")
+        return initiallyDraggedElement?.let {
+            val startOffset = it.offset.y + draggedDistance.y
+            val endOffset = it.offset.y + it.size.height + draggedDistance.y
 
-            val diffToEnd = endOffset.y - lazyListState.layoutInfo.viewportEndOffset
-            val diffToStart = startOffset.y - lazyListState.layoutInfo.viewportStartOffset
-            return when {
-                draggedDistance.y > 0 && diffToEnd > 0 -> diffToEnd.toFloat()
-                draggedDistance.y < 0 && diffToStart < 0 -> diffToStart.toFloat()
-                else -> 0f
+            Timber.d("checkForOverScroll startOffset = $startOffset endOffset = $endOffset viewPortStart = ${lazyListState.layoutInfo.viewportStartOffset} viewPortEnd = ${lazyListState.layoutInfo.viewportEndOffset}")
+            return@let when {
+                draggedDistance.y > 0 -> {
+                    Timber.d("checkForOverScroll draggedDistance.y > 0 ${endOffset - lazyListState.layoutInfo.viewportEndOffset}")
+                    (endOffset - lazyListState.layoutInfo.viewportEndOffset).takeIf { diff -> diff > 0 }
+                }
+                draggedDistance.y < 0 -> {
+                    Timber.d("checkForOverScroll draggedDistance.y < 0 ${startOffset - lazyListState.layoutInfo.viewportStartOffset}")
+                    (startOffset - lazyListState.layoutInfo.viewportStartOffset).takeIf { diff -> diff < 0 }
+                }
+                else -> null
             }
-        }
-        return 0f
+        } ?: 0f
     }
 
     fun getLazyListState(): LazyStaggeredGridState {
