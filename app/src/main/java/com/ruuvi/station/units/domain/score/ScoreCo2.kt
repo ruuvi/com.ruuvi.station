32f1259aa2507bf0d72759d44fbc078c6292defd
Denis Andreev <andreev.denis@gmail.com>
2025-09-19 22:28:36 +0300

    CO2/PM ranges fix

--- app/src/main/java/com/ruuvi/station/units/domain/score/ScoreCo2.kt
+++ app/src/main/java/com/ruuvi/station/units/domain/score/ScoreCo2.kt
@@ -3,10 +3,10 @@ package com.ruuvi.station.units.domain.score
 object ScoreCo2: IScoreMeasurement {
     override fun score(value: Double): QualityRange {
         return when {
-            value < 600 -> QualityRange.Excellent
-            value < 800 -> QualityRange.Good
-            value < 1350 -> QualityRange.Fair
-            value < 2100 -> QualityRange.Poor
+            value <= 600 -> QualityRange.Excellent
+            value <= 800 -> QualityRange.Good
+            value <= 1400 -> QualityRange.Fair
+            value <= 2100 -> QualityRange.Poor
             else -> QualityRange.VeryPoor
         }
     }
--- app/src/main/java/com/ruuvi/station/units/domain/score/ScorePM.kt
+++ app/src/main/java/com/ruuvi/station/units/domain/score/ScorePM.kt
@@ -3,10 +3,10 @@ package com.ruuvi.station.units.domain.score
 object ScorePM: IScoreMeasurement {
     override fun score(value: Double): QualityRange {
         return when {
-            value < 5 -> QualityRange.Excellent
-            value < 12 -> QualityRange.Good
-            value < 30 -> QualityRange.Fair
-            value < 55 -> QualityRange.Poor
+            value <= 6 -> QualityRange.Excellent
+            value <= 12 -> QualityRange.Good
+            value <= 30 -> QualityRange.Fair
+            value <= 55 -> QualityRange.Poor
             else -> QualityRange.VeryPoor
         }
     }
