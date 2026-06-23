bc9dac720f73713d910428d10933bd2fc48b2241
Mohammad Aqib Hassan <h_aqib11@rocketmail.com>
2026-06-24 02:53:42 +0300

    based on screen size widget dynamic fonts has been handled

---
+++ app/src/main/java/com/ruuvi/station/widgets/ui/WidgetScreenSizeCategory.kt
@@ -0,0 +1,28 @@
+package com.ruuvi.station.widgets.ui
+
+import android.content.Context
+import android.os.Build
+import android.view.WindowManager
+
+enum class WidgetScreenSizeCategory {
+    SMALL,
+    MEDIUM,
+    BIG
+}
+
+fun resolveWidgetScreenSizeCategory(context: Context): WidgetScreenSizeCategory {
+    val windowManager = context.getSystemService(WindowManager::class.java)
+    val screenHeightPx = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R && windowManager != null) {
+        windowManager.currentWindowMetrics.bounds.height()
+    } else {
+        context.resources.displayMetrics.heightPixels
+    }
+    val density = context.resources.displayMetrics.density
+    val screenHeightDp = screenHeightPx / density
+
+    return when {
+        screenHeightDp >= 900f -> WidgetScreenSizeCategory.BIG
+        screenHeightDp >= 760f -> WidgetScreenSizeCategory.MEDIUM
+        else -> WidgetScreenSizeCategory.SMALL
+    }
+}
