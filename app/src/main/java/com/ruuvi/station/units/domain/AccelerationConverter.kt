fe50c32f50bfda663027196fcac6f8a48e1117d4
Mohammad Aqib Hassan <h_aqib11@rocketmail.com>
2026-08-06 14:20:41 +0600

    Fix: removed outdated code on task -> more info on sensor settings to respect resolution #1616

--- app/src/main/java/com/ruuvi/station/units/domain/AccelerationConverter.kt
+++ app/src/main/java/com/ruuvi/station/units/domain/AccelerationConverter.kt
@@ -4,23 +4,6 @@ import android.content.Context
 import com.ruuvi.station.R
 
 class AccelerationConverter (val context: Context) {
-
-    fun getAccelerationString(acceleration: Double?, accelerationAxis: AccelerationAxis?): String {
-        return if (acceleration == null) {
-            UnitsConverter.NO_VALUE_AVAILABLE
-        } else {
-            context.getString(R.string.acceleration_reading, acceleration, getAccelerationUnit(accelerationAxis))
-        }
-    }
-
-    fun getAccelerationStringWithoutUnit(acceleration: Double?): String {
-        return if (acceleration == null) {
-            UnitsConverter.NO_VALUE_AVAILABLE
-        } else {
-            context.getString(R.string.acceleration_reading, acceleration, "").trim()
-        }
-    }
-
     fun getAccelerationUnit(accelerationAxis: AccelerationAxis?): String =
         context.getString(accelerationAxis?.unitCode ?: R.string.acceleration_unit)
 }
