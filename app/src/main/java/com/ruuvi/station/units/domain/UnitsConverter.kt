a95e38f0ee041af43fe7424fec31e45671d6ba66
Mohammad Aqib Hassan <AchillesSium@users.noreply.github.com>
2026-09-22 12:32:05 +0300

    fix: absolute humidity alert doesn't trigger if the range is below latest measured #1633

--- app/src/main/java/com/ruuvi/station/units/domain/UnitsConverter.kt
+++ app/src/main/java/com/ruuvi/station/units/domain/UnitsConverter.kt
@@ -317,7 +317,7 @@ class UnitsConverter (
             if (it !in -100.0..370.0) {
                 null
             } else {
-                HumidityConverter(temperature, humidity / 100)
+                HumidityConverter(temperature, humidity)
             }
         } ?: return null
         return when (humidityUnit) {
