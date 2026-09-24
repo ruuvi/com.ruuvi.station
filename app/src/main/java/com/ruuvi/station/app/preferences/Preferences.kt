f97922696debfec4c867282dd89577ef9def7979
Mohammad Aqib Hassan <h_aqib11@rocketmail.com>
2026-09-24 15:35:59 +0300

    fix: removed PREF_BACKGROUND_SCAN_INTERVAL_LAST_UPDATED and PREF_BACKGROUND_SCAN_MODE_LAST_UPDATED (sync relared all issue)

--- app/src/main/java/com/ruuvi/station/app/preferences/Preferences.kt
+++ app/src/main/java/com/ruuvi/station/app/preferences/Preferences.kt
@@ -513,9 +513,7 @@ class Preferences (val context: Context) {
     fun resetAppSettings() {
         sharedPreferences.edit {
             remove(PREF_BACKGROUND_SCAN_INTERVAL)
-            remove(PREF_BACKGROUND_SCAN_INTERVAL_LAST_UPDATED)
             remove(PREF_BACKGROUND_SCAN_MODE)
-            remove(PREF_BACKGROUND_SCAN_MODE_LAST_UPDATED)
             remove(PREF_TEMPERATURE_UNIT)
             remove(PREF_TEMPERATURE_UNIT_LAST_UPDATED)
             remove(PREF_HUMIDITY_UNIT)
